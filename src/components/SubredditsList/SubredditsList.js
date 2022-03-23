81eec3c331752c6f9f997fcdd8b0ac87ce4287d9
istvankaszab <istvan.kaszab@gmail.com>
2022-03-23 12:53:27 +0000

    Change Subreddits to SubredditsList

---
+++ src/components/SubredditsList/SubredditsList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './SubredditsList.css'
+
+function SubredditsList() {
+  return (
+    <aside>
+      <h2>Subreddits List</h2>
+    </aside>
+  )
+}
+
+export default SubredditsList
